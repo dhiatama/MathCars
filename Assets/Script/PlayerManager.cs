028423fa52246ac5e999f264b93455337d311c5b
dhiatama <102406271+dhiatama@users.noreply.github.com>
2023-07-06 22:50:37 +0700

    Game Step 9-10

---
+++ Assets/Script/PlayerManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour 
+{
+	public static bool gameOver;
+	public GameObject gameOverPanel;
+
+	public static bool isGameStarted;
+	public GameObject startingText;
+	void Start () 
+	{
+		gameOver = false;
+		Time.timeScale = 1;
+		isGameStarted = false;
+	}
+	
+
+	void Update () 
+	{
+		if (gameOver) 
+		{
+			Time.timeScale = 0;
+			gameOverPanel.SetActive (true);
+		}
+
+		if (SwipeManager.tap) 
+		{
+			isGameStarted = true;
+			Destroy(startingText);
+		}
+	}
+}
